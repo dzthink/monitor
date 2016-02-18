415b49051b09ad7ab1c1b8717200b97852cd849f
dzthink <dzthink@qq.com>
2016-02-18 09:49:39 +0800

    提供http api接口

---
+++ src/web/api.go
@@ -0,0 +1,25 @@
+package web
+
+import(
+    "data"
+    "net/http"
+)
+type ApiServer struct {
+    stg *data.Storage
+    server *http.Server
+}
+
+func NewApiServer(option *option.Option, stg *data.Storage) *ApiServer {
+
+}
+
+func(apiServer *ApiServer)Serve() {
+
+}
+
+func(apiServer *ApiServer)route() map[string]func() {
+    route := make(map[string]func())
+    route["/health/url"] = func(w http.ResponseWriter, r *http.Request){
+
+    }
+}
